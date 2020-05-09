8105a19abe8e70804bf97cac5fc54851b3815066
Sj1218305 <jainshobit0@gmail.com>
2020-05-09 23:42:43 +0530

    added getallmessage route

--- routes/message.js
+++ routes/message.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const MessageCtrl = require('../controllers/message');
 const auth = require('../middleware/guideAuth');
 
+router.get('/guide/chat-messages/:sender_Id/:receiver_Id',auth,MessageCtrl.GetAllMessages)
 
 router.post('/guide/chat-messages/:sender_Id/:receiver_Id',auth,MessageCtrl.SendMessage)
 
