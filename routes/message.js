9c9005032e35ae5b5b477f72eb73473560d1c670
Sj1218305 <jainshobit0@gmail.com>
2020-05-11 13:24:19 +0530

    added tourist route

--- routes/message.js
+++ routes/message.js
@@ -4,9 +4,15 @@ const router = express.Router();
 
 const MessageCtrl = require('../controllers/message');
 const auth = require('../middleware/guideAuth');
+const auth1 = require('../middleware/touristAuth');
+
 
 router.get('/guide/chat-messages/:sender_Id/:receiver_Id',auth,MessageCtrl.GetAllMessages)
 
 router.post('/guide/chat-messages/:sender_Id/:receiver_Id',auth,MessageCtrl.SendMessage)
+
+router.post('/tourist/chat-messages/:sender_Id/:receiver_Id',auth1,MessageCtrl.SendMessage)
+
+
 
 module.exports = router;
