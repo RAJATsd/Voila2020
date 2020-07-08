ca0f05671882c8cc2f9e806ce0216b9e97b7862c
RAJATsd <rajatsharmaatri@gmail.com>
2020-07-08 12:49:12 +0530

    error handeling p1

--- controllers/authGuide.js
+++ controllers/authGuide.js
@@ -3,100 +3,162 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 exports.postSignup = (req,res,next) => {
-    const userData = JSON.parse(req.body.data);
-    const phoneNumber = userData.phoneNumber;
-    // const profilePic = req.file;
-    // if(!profilePic)
-    // {
-    //     return res.json({message:"no file uploaded"});
-    // }
-    Guide.findOne({phoneNumber:phoneNumber})
-    .then(result => {
-        if(result)
-        {
-            res.json({message:"Guide Already Exist"});
-        }
-        else{
-            //const picUrl = 'localhost:3000/profileImages/'+profilePic.filename;
-            const password = userData.password;
-            bcrypt.hash(password,8)
-            .then(hashed=>{
-                const newGuide = new Guide({
-                    name : userData.name,
-                    gender : userData.gender, 
-                    password : hashed,
-                    dob : userData.dob,
-                    phoneNumber : phoneNumber,
-                    email : userData.email,
-                    address : userData.address,
-                    experience : userData.experience,
-                    peopleLimit : userData.peopleLimit,
-                    perHeadCharge : userData.perHeadCharge,
-                    perDayCharge : userData.perDayCharge,
-                    //picUrl : picUrl,
-                    aadhaarNumber : userData.aadhaarNumber,
-                    interests : userData.interests,
-                    languages : userData.languages,
-                    city : userData.city,
-                    state : userData.state
-                });    
-                newGuide.save()
-                .then(result =>{
-                    res.status(201).json({message:"New Guide Registered Successfully",data : result});
+    try{
+        const userData = JSON.parse(req.body.data);
+        const phoneNumber = userData.phoneNumber;
+        // const profilePic = req.file;
+        // if(!profilePic)
+        // {
+        //     return res.json({message:"no file uploaded"});
+        // }
+        Guide.findOne({phoneNumber:phoneNumber})
+        .then(result => {
+            if(result)
+            {
+                res.json({
+                    success:false,
+                    message:"Guide Already Exist. Please login"
+                });
+            }
+            else{
+                //const picUrl = 'localhost:3000/profileImages/'+profilePic.filename;
+                const password = userData.password;
+                bcrypt.hash(password,8)
+                .then(hashed=>{
+                    const newGuide = new Guide({
+                        name : userData.name,
+                        gender : userData.gender, 
+                        password : hashed,
+                        dob : userData.dob,
+                        phoneNumber : phoneNumber,
+                        email : userData.email,
+                        address : userData.address,
+                        experience : userData.experience,
+                        peopleLimit : userData.peopleLimit,
+                        perHeadCharge : userData.perHeadCharge,
+                        perDayCharge : userData.perDayCharge,
+                        //picUrl : picUrl,
+                        aadhaarNumber : userData.aadhaarNumber,
+                        interests : userData.interests,
+                        languages : userData.languages,
+                        city : userData.city,
+                        state : userData.state
+                    });    
+                    newGuide.save()
+                    .then(result =>{
+                        res.status(201).json({
+                            success:true,
+                            message:"New Guide Registered Successfully",
+                            data : result
+                        });
+                    })
+                    .catch(err=>{
+                        console.log(err);
+                        res.json({
+                            success:false,
+                            message:"INTERNAL SERVER ERROR"
+                        })
+                    });
                 })
                 .catch(err=>{
-                    console.log(err);
+                    console.log(err)
+                    res.json({
+                        success:false,
+                        message:'INTERNAL SERVER ERROR'
+                    });
                 });
-            })
-            .catch(err=>{
-                console.log(err);
+            }
+        })
+        .catch(err=>{
+            console.log(err);
+            res.json({
+                success:false,
+                message:'INTERNAL SERVER ERROR'
             });
-        }
-    })
-    .catch(err=>{
-        console.log(err);
-    });
+        });
+    
+    }
+    catch(e){
+        console.log(e);
+        res.json({
+            success:false,
+            message : "INTERNAL SERVER ERROR"
+        })
+    }
 }
 
 exports.postLogin = async(req,res,next) => {
-    const email = req.body.email;
-    const password = req.body.password;
-
-    Guide.findOne({email:email})
-    .populate('chatList.receiverId')
-    .populate('chatList.msgId')
-    .then(guide => {
-        if(!guide)
-        {
-            res.status(417).json({message:"No such guide exist,please signup if you are a new guide"});
-        }
-        else{
-            bcrypt.compare(password,guide.password)
-            .then(matchingPassword => {
-                if(!matchingPassword)
-                {
-                    res.status(406).json({message:"Password do not match, please try again"});
-                }
-                else{
-                    const token = jwt.sign({email:email,_id:guide._id.toString()},'thisismysecretkeyforthishackathon2020',{expiresIn:'5h'});
-                    guide.tokens = guide.tokens.concat({token});
-                    guide.save()
-                    .then(saved => {
-                        res.status(200).json({message:"Person successfully logged in",token:token,guide:guide});
-                    })
-                    .catch(errorWhileSave => {
-                        console.log(errorWhileSave);
-                    });    
-                }
-            })
-            .catch(err=>{
-                console.log(err);
-            });
-        }
-    })
-    .catch(err => {
-        console.log(err);
-    });
+    try{
+        const email = req.body.email;
+        const password = req.body.password;
+    
+        Guide.findOne({email:email})
+        .populate('chatList.receiverId')
+        .populate('chatList.msgId')
+        .then(guide => {
+            if(!guide)
+            {
+                res.status(417).json({
+                    success:false,
+                    message:"Email or Password incorrect ,please signup if you are a new guide"
+                });
+            }
+            else{
+                bcrypt.compare(password,guide.password)
+                .then(matchingPassword => {
+                    if(!matchingPassword)
+                    {
+                        res.status(406).json({
+                            success:false,
+                            message:"Email or Password incorrect ,please signup if you are a new guide"
+                        });
+                    }
+                    else{
+                        const token = jwt.sign({email:email,_id:guide._id.toString()},'thisismysecretkeyforthishackathon2020',{expiresIn:'5h'});
+                        guide.tokens = guide.tokens.concat({token});
+                        guide.save()
+                        .then(saved => {
+                            res.status(200).json({
+                                success:true,
+                                message:"Person successfully logged in",
+                                token:token,
+                                guide:guide
+                            });
+                        })
+                        .catch(errorWhileSave => {
+                            console.log(errorWhileSave);
+                            res.json({
+                                success:false,
+                                message:"INTERNAL SERVER ERROR"
+                            });
+                        });    
+                    }
+                })
+                .catch(err=>{
+                    console.log(err);
+                    res.json({
+                        success:false,
+                        message:"INTERNAL SERVER ERROR"
+                    });
+                });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.json({
+                success:false,
+                message:"INTERNAL SERVER ERROR"
+            });        
+        });
+    
+    }
+    catch(e){
+        console.log(e);
+        res.json({
+            success:false,
+            message:"INTERNAL SERVER ERROR"
+        })
+    }
 }
 
 exports.getLogout = async (req,res,next) => {
@@ -106,10 +168,17 @@ exports.getLogout = async (req,res,next) => {
             return token.token != req.token;
         });
         await req.user.save();
-        res.json({message:"Successfully logged out"});
+        res.json({
+            success:true,
+            message:"Successfully logged out"
+        });
     }
-    catch(error) {
-        res.status(500).json({message:"some error occured",error:error});
+    catch(error){
+        console.log(error);
+        res.status(500).json({
+            success:false,
+            message:"INTERNAL SERVER ERROR"
+        });
     }
 }
 
@@ -119,14 +188,24 @@ exports.getLogoutAll = (req,res,next) => {
         req.user.tokens.splice(0,req.user.tokens.length);
         req.user.save()
         .then(saved => {
-            res.json({message:"logged out from all devices"});
+            res.json({
+                success:true,
+                message:"logged out from all devices"
+            });
         })
         .catch(errorWhileSave => {
             console.log(errorWhileSave);
+            res.json({
+                success:false,
+                message:'INTERNAL SERVER ERROR'
+            })
         });
     }
     catch(error)
     {
-        res.status(500).json({message:"some error occured",error:error});
+        res.status(500).json({
+            success:false,
+            message:"INTERNAL SERVER ERROR"
+        });
     }
 }
--- controllers/authTourist.js
+++ controllers/authTourist.js
@@ -3,85 +3,150 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 exports.postSignup = (req,res,next) => {
-    const phoneNumber = req.body.phoneNumber;
-    Tourist.findOne({phoneNumber:phoneNumber})
-    .then(result => {
-        if(result)
-        {
-            res.json({message:"Guide Already Exist"});
-        }
-        else{
-            const password = req.body.password;
-            bcrypt.hash(password,12)
-            .then(hashed=>{
-                const newTourist = new Tourist({
-                    name : req.body.name,
-                    gender : req.body.gender, 
-                    password : hashed,
-                    dob : req.body.dob,
-                    phoneNumber : phoneNumber,
-                    email : req.body.email,
-                    interests : req.body.interests,
-                    languages : req.body.languages,
-                    nationality:req.body.nationality,
-                });    
-                newTourist.save()
-                .then(result =>{
-                    res.status(201).json({message:"New Tourist Registered Successfully",data : result});
+    try{
+        const phoneNumber = req.body.phoneNumber;
+        Tourist.findOne({phoneNumber:phoneNumber})
+        .then(result => {
+            if(result)
+            {
+                res.json({
+                    success:false,
+                    message:"Guide Already Exist. Please login"
+                });
+            }
+            else{
+                const password = req.body.password;
+                bcrypt.hash(password,12)
+                .then(hashed=>{
+                    const newTourist = new Tourist({
+                        name : req.body.name,
+                        gender : req.body.gender, 
+                        password : hashed,
+                        dob : req.body.dob,
+                        phoneNumber : phoneNumber,
+                        email : req.body.email,
+                        interests : req.body.interests,
+                        languages : req.body.languages,
+                        nationality:req.body.nationality,
+                    });    
+                    newTourist.save()
+                    .then(result =>{
+                        res.status(201).json({
+                            success:true,
+                            message:"New Tourist Registered Successfully",
+                            data : result
+                        });
+                    })
+                    .catch(err=>{
+                        console.log(err);
+                        res.json({
+                            success:false,
+                            message:"INTERNAL SERVER ERROR"
+                        });
+                    });
                 })
                 .catch(err=>{
                     console.log(err);
+                    res.json({
+                        success:false,
+                        message:"INTERNAL SERVER ERROR"
+                    });
                 });
-            })
-            .catch(err=>{
-                console.log(err);
+            }
+        })
+        .catch(err=>{
+            console.log(err);
+            res.json({
+                success:false,
+                message:"INTERNAL SERVER ERROR"
             });
-        }
-    })
-    .catch(err=>{
-        console.log(err);
-    });
+        });
+    }
+    catch(e){
+        console.log(e);
+        res.json({
+            success:false,
+            message:"INTERNAL SERVER ERROR"
+        });
+    }
 }
 
 exports.postLogin = async(req,res,next) => {
-    const email = req.body.email;
-    const password = req.body.password;
-
-    Tourist.findOne({email:email})
-    .populate('chatList.receiverId')
-    .populate('chatList.msgId')
-    .then(result => {
-        if(!result)
-        {
-            res.status(417).json({message:"No such Tourist exist,please signup if you are a new Tourist"});
-        }
-        else{
-            bcrypt.compare(password,result.password)
-            .then(matchingPassword => {
-                if(!matchingPassword)
-                {
-                    res.status(406).json({message:"Password do not match, please try again"});
-                }
-                else{
-                    const token = jwt.sign({email:email,_id:result._id.toString()},'thisismysecretkeyforthishackathon2020',{expiresIn:'5h'});
-                    result.tokens = result.tokens.concat({token});
-                    result.save()
-                    .then(saved => {
-                        res.status(200).json({message:"Person successfully logged in",token:token,Tourist:result});
-                    })
-                    .catch(errorWhileSave => {
-                        console.log(errorWhileSave);
+    try{
+        const email = req.body.email;
+        const password = req.body.password;
+    
+        Tourist.findOne({email:email})
+        .populate('chatList.receiverId')
+        .populate('chatList.msgId')
+        .then(result => {
+            if(!result)
+            {
+                res.status(417).json({
+                    success:false,
+                    message:"No such Tourist exist,please signup if you are a new Tourist"
+                });
+            }
+            else{
+                bcrypt.compare(password,result.password)
+                .then(matchingPassword => {
+                    if(!matchingPassword)
+                    {
+                        res.status(406).json({
+                            success:false,
+                            message:"Email or Password do not match, please try again"
+                        });
+                    }
+                    else{
+                        const token = jwt.sign({
+                            email:email,
+                            _id:result._id.toString()},
+                            'thisismysecretkeyforthishackathon2020',
+                            {expiresIn:'5h'});
+                        result.tokens = result.tokens.concat({token});
+                        result.save()
+                        .then(saved => {
+                            res.status(200).json({
+                                success:true,
+                                message:"Person successfully logged in",
+                                token:token,
+                                Tourist:result
+                            });
+                        })
+                        .catch(errorWhileSave => {
+                            console.log(errorWhileSave);
+                            res.json({
+                                success:false,
+                                message:"INTERNAL SERVER ERROR"
+                            });
+                        });
+                    }
+                })
+                .catch(err=>{
+                    console.log(err);
+                    res.json({
+                        success:false,
+                        message:"INTERNAL SERVER ERROR"
                     });
-                }
-            })
-            .catch(err=>{
-                console.log(err);
+                });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.json({
+                success:false,
+                message:"INTERNAL SERVER ERROR"
             });
-        }
-    })
-    .catch(err => {
-        console.log(err);
-    });
+        });
+    
+    }
+    catch(e){
+        console.log(e);
+        res.json({
+            success:false,
+            message:"INTERNAL SERVER ERROR"
+        });
+    }
 }
 
 exports.getLogout = async (req,res,next) => {
@@ -90,10 +155,17 @@ exports.getLogout = async (req,res,next) => {
             return token.token != req.token;
         });
         await req.user.save();
-        res.json({message:"Successfully logged out"});
+        res.json({
+            success:true,
+            message:"Successfully logged out"
+        });
     }
     catch(error) {
-        res.status(500).json({message:"some error occured",error:error});
+        console.log(error)
+        res.status(500).json({
+            success:false,
+            message:"INTERNAL SERVER ERROR"
+        });
     }
 }
 
@@ -103,14 +175,24 @@ exports.getLogoutAll = (req,res,next) => {
         req.user.tokens.splice(0,req.user.tokens.length);
         req.user.save()
         .then(saved => {
-            res.json({message:"Successfully logged out from all devices"});
+            res.json({
+                success:true,
+                message:"Successfully logged out from all devices"
+            });
         })
         .catch(errorWhileSave => {
             console.log(errorWhileSave);
+            res.json({
+                success:false,
+                message:"INTERNAL SERVER ERROR"
+            });
         });
     }
     catch(error)
     {
-        res.status(500).json({message:"some error occured",error:error});
+        res.json({
+            success:false,
+            message:"INTERNAL SERVER ERROR"
+        });
     }
 }       
