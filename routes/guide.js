afe5a93eec1bfa17b128142dfad568e478f22018
Sj1218305 <jainshobit0@gmail.com>
2020-07-13 23:37:46 +0530

    add chat list route

--- routes/guide.js
+++ routes/guide.js
@@ -31,4 +31,7 @@ router.put('/guide/profile/changePassword',auth,commonController.changePassword)
 router.get('/getUserByEmail/:role/:email',auth,commonController.getUserByEmail);
 //gets user with provided email
 //requirements : email and role in params
+router.get('/guide/messages/:id/',guideController.showList);
+//gets list of tour guide chat with his/her tourists
+//requirements : guideId in params
 module.exports = router;
