3442782895ed88aba3a4366decd5c85ac2ae4b0f
Sj1218305 <jainshobit0@gmail.com>
2020-07-21 12:40:52 +0530

    add joinRoom and message event

---
+++ socket/room.js
@@ -0,0 +1,30 @@
+const room = require('../models/room');
+
+module.exports = function(io){
+	io.on('connection', socket =>{
+		
+		//joinRoom event
+		socket.on('joinRoom',async(data) => {
+		//console.log(data);
+		const roomDetails = await room.findById({
+		_id: data.roomId
+	});
+		
+		socket.join(roomDetails.name);
+		});
+	
+	//sendMessage event
+	socket.on('message',async(data) => {
+		const roomDetails = await room.findById({
+		_id: data.roomId
+	});
+		 // console.log(roomDetails.name);
+		var msg = data.message;
+		roomDetails.chatList.push({
+			senderId : data.senderId,
+			body: msg
+		});
+		 socket.to(roomDetails.name).emit("message",{msg});
+	});
+	});
+};
